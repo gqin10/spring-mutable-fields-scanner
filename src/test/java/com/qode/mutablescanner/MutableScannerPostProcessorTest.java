20c58e6ae67a3d93f7fed8f9bee97ee0fb94e661
gq <qin.001010@gmail.com>
2026-04-04 13:58:09 +0800

    fix: unit test context creation

--- src/test/java/com/qode/mutablescanner/MutableScannerPostProcessorTest.java
+++ src/test/java/com/qode/mutablescanner/MutableScannerPostProcessorTest.java
@@ -16,8 +16,10 @@ public class MutableScannerPostProcessorTest {
 
     @Test
     public void beanHasNoMutableFields_expectSuccess() {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ExpectSuccessConfig.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(ExpectSuccessConfig.class);
         context.scan(AbstractTestConfig.IMPL_PACKAGES);
+        context.refresh();
         Assertions.assertNotNull(context.getBean(MutableScannerPostProcessor.class));
         Assertions.assertNotNull(context.getBean(ChildServiceB.class));
         Assertions.assertNotNull(context.getBean(ChildServiceA.class));
@@ -25,8 +27,10 @@ public class MutableScannerPostProcessorTest {
 
     @Test
     public void testNoBeanForNoScanPackage_expectSuccess() {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ExpectSuccessConfig.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(ExpectSuccessConfig.class);
         context.scan(AbstractTestConfig.IMPL_PACKAGES);
+        context.refresh();
         Exception exception = null;
         try {
             Assertions.assertNull(context.getBean(NoScanService.class));
@@ -42,8 +46,10 @@ public class MutableScannerPostProcessorTest {
     public void beanHasMutableField_expectFail() {
         Exception exception = null;
         try {
-            AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(HasMutableExpectFailConfig.class);
+            AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+            context.register(HasMutableExpectFailConfig.class);
             context.scan(AbstractTestConfig.IMPL_PACKAGES, AbstractTestConfig.NOSCAN_PACKAGES, AbstractTestConfig.SCAN_MUTABLE_PACKAGES);
+            context.refresh();
             Assertions.assertNotNull(context.getBean(MutableScannerPostProcessor.class));
             ScanHasMutableService scanHasMutableService = context.getBean(ScanHasMutableService.class);
         } catch (Exception e) {
@@ -57,15 +63,19 @@ public class MutableScannerPostProcessorTest {
 
     @Test
     public void testBeanOutsideScanPackage_beanHasMutableField_expectSuccess() {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(BeanOutsideScanPackageExpectSuccessConfig.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(BeanOutsideScanPackageExpectSuccessConfig.class);
         context.scan(AbstractTestConfig.IMPL_PACKAGES); // does not scan noscanimpl
+        context.refresh();
         Assertions.assertNotNull(context.getBean(MutableScannerPostProcessor.class));
     }
 
     @Test
     public void testAllowMutableAnnotation_expectSuccess() {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ExpectSuccessConfig.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(ExpectSuccessConfig.class);
         context.scan(AbstractTestConfig.IMPL_PACKAGES);
+        context.refresh();
         Assertions.assertEquals(0, context.getBeansOfType(NoScanService.class).size());
 
         HasAllowMutableFieldService hasMutableService = context.getBean(HasAllowMutableFieldService.class);
@@ -76,8 +86,10 @@ public class MutableScannerPostProcessorTest {
 
     @Test
     public void testContinueOnMutableFieldFoundConfig_expectNoError() {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ContinueOnMutableFieldFoundConfig.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(ContinueOnMutableFieldFoundConfig.class);
         context.scan(AbstractTestConfig.IMPL_PACKAGES, AbstractTestConfig.NOSCAN_PACKAGES, AbstractTestConfig.SCAN_MUTABLE_PACKAGES);
+        context.refresh();
         MutableScannerPostProcessor postProcessor = context.getBean(MutableScannerPostProcessor.class);
         Assertions.assertNotNull(postProcessor);
         Assertions.assertNotNull(context.getBean(ScanHasMutableService.class));
@@ -90,8 +102,10 @@ public class MutableScannerPostProcessorTest {
     public void testConfigureNotContinueOnMutableFieldFound_expectException() {
         Exception exception = null;
         try {
-            AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(HasMutableExpectFailConfig.class);
+            AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+            context.register(HasMutableExpectFailConfig.class);
             context.scan(AbstractTestConfig.IMPL_PACKAGES, AbstractTestConfig.NOSCAN_PACKAGES, AbstractTestConfig.SCAN_MUTABLE_PACKAGES);
+            context.refresh();
             Assertions.assertNotNull(context.getBean(MutableScannerPostProcessor.class));
             ScanHasMutableService scanHasMutableService = context.getBean(ScanHasMutableService.class);
         } catch (Exception e) {
